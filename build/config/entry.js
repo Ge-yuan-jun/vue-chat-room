a31918bbe81be1a3d04ffc30b0eec71bb0bf447c
GeYuanjun <daydaygyj@gmail.com>
2017-03-03 19:01:53 +0800

    add entry

---
+++ build/config/entry.js
@@ -0,0 +1,13 @@
+export default {
+    vendor: [
+        'vue',
+        'vue-router',
+        'vuex',
+        'vuex-router-sync',
+        'axios',
+    ],
+    app: [ // app
+        './main.js', // 入口
+        './global/utils', // 工具类
+    ]
+}

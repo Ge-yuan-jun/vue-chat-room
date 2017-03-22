1b1aa6af985beebc9b6178da66f9614319482d3c
GeYuanjun <daydaygyj@gmail.com>
2017-03-22 10:48:57 +0800

    add file

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,14 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="chat">
+    <div class="sidebar">
+      <div class="card">
+        <header>
+          <img class="avatar" width="40" height="40" alt="墨白" src="../assets/header.jpeg">
+        </header>
+        <footer></footer>
+      </div>
+    </div>
+    <div class="main"></div>
   </div>
 </template>
 
@@ -32,22 +24,36 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="sass" scoped>
+.chat {
+  margin: 20px auto;
+  width: 800px;
+  height: 600px;
+  overflow: hidden;
+  border-radius: 3px;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  .sidebar, .main {
+    height: 100%;
+  }
+  .sidebar {
+    float: left;
+    width: 200px;
+    color: #f4f4f4;
+    background-color: #2e3238;
+  }
+  .main {
+    position: relative;
+    overflow: hidden;
+    background-color: #eee;
+  }
+  .text {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+  }
+  .message {
+    height: 'calc(100% - 160px)';
+  }
 }
 </style>
